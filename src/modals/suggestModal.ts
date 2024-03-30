43bf3b49afca02b35767df1e10ef2dda843d9d8e
Prisha Gupta <prishapg101@gmail.com>
2024-03-30 22:08:49 +0530

    Added modal for `/rcc-suggest` command

---
+++ src/modals/suggestModal.ts
@@ -0,0 +1,76 @@
+import {
+    IHttp,
+    IModify,
+    IRead,
+    IUIKitSurfaceViewParam,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import {
+    IUIKitSurface,
+    UIKitSurfaceType,
+} from "@rocket.chat/apps-engine/definition/uikit";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { getButton, getInputBox } from "../utils/blockBuilders";
+import { handleCommandResponse } from "../utils/handleCommandResponse";
+import { requestServer } from "../utils/requestServer";
+
+export const COMMAND = "rcc-suggest";
+export const SUGGEST_COMMAND_MODAL = "suggest-command";
+
+export async function suggestModal(): Promise<IUIKitSurfaceViewParam> {
+    return {
+        id: SUGGEST_COMMAND_MODAL,
+        type: UIKitSurfaceType.MODAL,
+        title: {
+            type: "plain_text",
+            text: "Get Suggestions",
+        },
+        close: await getButton("Close", "", ""),
+        clearOnClose: true,
+        submit: await getButton("Submit", "submit", "submit", "Submit"),
+        blocks: [
+            await getInputBox(
+                "",
+                "What code you want to get suggestions for?",
+                "suggest",
+                "suggest",
+                "",
+                true
+            ),
+        ],
+    };
+}
+
+export async function suggestModalSubmitHandler(
+    view: IUIKitSurface,
+    sender: IUser,
+    room: IRoom,
+    read: IRead,
+    modify: IModify,
+    http: IHttp
+) {
+    const state = view.state as Record<string, any> | undefined;
+    console.log(state);
+    if (!state) return;
+
+    const query = state.suggest.suggest;
+    const sendMessage = await handleCommandResponse(
+        "\n```\n" + query + "\n```",
+        sender,
+        room,
+        modify,
+        COMMAND
+    );
+
+    const res = await requestServer(http, "/suggest", { query });
+    if (!res) {
+        await sendMessage(
+            "❌ Failed to get suggestions. Please try again later."
+        );
+        return;
+    }
+
+    const data = res as Record<string, string>;
+
+    await sendMessage(data.result);
+}
